a4c2254ff22a10c1868a3105225ae9e4b93b055d
ankitabaddoli4 <ankitabaddoli4@gmail.com>
2026-04-24 16:26:36 +0530

    Add files via upload

---
+++ src/models/task_models.py
@@ -0,0 +1,12 @@
+class Task:
+    def __init__(self, id, title, status):
+        self.id = id
+        self.title = title
+        self.status = status
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "status": self.status
+        }
